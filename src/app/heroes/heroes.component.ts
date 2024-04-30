af6148162efec963c3e0f094f99bfb88fbcf6514
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-05-01 00:07:26 +0300

    Добавил сервис

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
 import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -17,8 +17,18 @@ import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
