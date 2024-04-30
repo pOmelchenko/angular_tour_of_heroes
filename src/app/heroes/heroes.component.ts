88e2ec53eb84ea42df04ba0aacc2df0d6705332e
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-04-30 23:55:40 +0300

    Добавил компонент героев

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {HEROES} from "../mock-heroes";
+import {Hero} from "../hero";
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
